810a9fc074bd7496237122a415d8a17bf5dc9761
Olof-Joachim Frahm <Olof.Frahm@web.de>
2009-10-25 23:14:07 +0100

    Exported symbols.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,17 @@
 (in-package #:cl-user)
 
 (defpackage cl-notify
-  (:use #:cl #:cffi))
+  (:use #:cl #:cffi)
+  (:export #:inotify-event
+	   #:inotify-event-wd
+	   #:inotify-event-mask
+	   #:inotify-event-cookie
+	   #:inotify-event-name
+
+	   #:inotify-read-raw-event
+	   #:inotify-read-event
+	   #:make-inotify
+	   #:close-notify
+	   #:watch
+	   #:unwatch
+	   ))
